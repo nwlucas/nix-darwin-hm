740d380633e325755c7ef14790002771435127a6
Nigel Williams-Lucas <nigel.williamslucas@gmail.com>
2024-03-31 20:58:53 -0400

    Adding in starship configurations

--- home/cli/starship/default.nix
+++ home/cli/starship/default.nix
@@ -17,9 +17,12 @@
       add_newline = false;
 
       character = {
-        success_symbol = "[>](bold prompt)";
-        error_symbol = "[>](bold red)";
-        vicmd_symbol = "[-](bold prompt)";
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[❯](bold red)";
+        vimcmd_symbol = "[❮](bold green)";
+        vimcmd_visual_symbol = "[❮](bold yellow)";
+        vimcmd_replace_symbol = "[❮](bold purple)";
+        vimcmd_replace_one_symbol = "[❮](bold purple)";
       };
 
       cmd_duration = {
