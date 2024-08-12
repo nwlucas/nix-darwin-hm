469ad06718cd97749bc3491cc466790a0b16be8e
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-08-12 17:56:32 -0400

    Trying to add GPG Sign to work profile

--- home/cli/git/default.nix
+++ home/cli/git/default.nix
@@ -80,11 +80,16 @@ in
             user = {
               email = "59927973+nwilliams-lucas@users.noreply.github.com";
               name = "Nigel Williams-Lucas";
+              signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMuboq7Wpr2+0SIZoq+MeGW2+5BcvOYnA0k5a6+rvqvC";
             };
 
             gpg = {
               format = "ssh";
               ssh.program = "/Applications/1Password.app/Contents/MacOS/op-sign-ssh";
+            };
+
+            commit = {
+              gpgsign = true;
             };
           };
         }
