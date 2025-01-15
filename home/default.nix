33f68cc2392899b389395323ea4881b183fb4955
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2025-01-14 20:40:57 -0500

    Working out permissions

--- home/default.nix
+++ home/default.nix
@@ -16,12 +16,57 @@ in
     ./cli
   ];
 
-  home.file."Cloudflare_CA.pem".source = ../files/certs/certificate.pem;
-  home.file.".curlrc".source = ../files/.curlrc;
-  home.file.".ssh/id_ed25519".source = ../files/personal.pub;
-  home.file.".ssh/glab-work".source = ../files/glab-work.pub;
-
   home = {
+    file = {
+      "Cloudflare_CA" = {
+        text = ''
+        -----BEGIN CERTIFICATE-----
+        MIIDHTCCAsOgAwIBAgIURCw5MNDWeKV9xjY641rGsPE7MGcwCgYIKoZIzj0EAwIw
+        gcAxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1T
+        YW4gRnJhbmNpc2NvMRkwFwYDVQQKExBDbG91ZGZsYXJlLCBJbmMuMRswGQYDVQQL
+        ExJ3d3cuY2xvdWRmbGFyZS5jb20xTDBKBgNVBAMTQ0dhdGV3YXkgQ0EgLSBDbG91
+        ZGZsYXJlIE1hbmFnZWQgRzEgMzE2YzBiYTk0MjlmMzFjMTRlZGFmNzBhNDgyMjA3
+        NjkwHhcNMjQxMjExMTkyOTAwWhcNMjkxMjExMTkyOTAwWjCBwDELMAkGA1UEBhMC
+        VVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBGcmFuY2lzY28x
+        GTAXBgNVBAoTEENsb3VkZmxhcmUsIEluYy4xGzAZBgNVBAsTEnd3dy5jbG91ZGZs
+        YXJlLmNvbTFMMEoGA1UEAxNDR2F0ZXdheSBDQSAtIENsb3VkZmxhcmUgTWFuYWdl
+        ZCBHMSAzMTZjMGJhOTQyOWYzMWMxNGVkYWY3MGE0ODIyMDc2OTBZMBMGByqGSM49
+        AgEGCCqGSM49AwEHA0IABPaQOVgkrzRB58TvmBomvaNH1cJoRZxtM1Z25eq/DnKZ
+        8q2NWZL+N91rzKRKyJ62xJNz+EgFMAyWTIneN69rhPCjgZgwgZUwDgYDVR0PAQH/
+        BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFH+i4Q5pdL5lRS/i3onS
+        aKqOnrecMFMGA1UdHwRMMEowSKBGoESGQmh0dHA6Ly9jcmwuY2xvdWRmbGFyZS5j
+        b20vM2U4ZTM0MTEtOTMxMC00ZTYxLTljNjUtYjUzOTEwZGNhODFhLmNybDAKBggq
+        hkjOPQQDAgNIADBFAiBIiZVkJjfztxzPkimJIKtbQdY+wOrg1OJWNTS1spah7AIh
+        AMLuNe00+2o61dUlmAACQHak55uSRHk99tRlP98Iv3gi
+        -----END CERTIFICATE-----
+        '';
+        target = "${homePrefix}/${user}/Cloudflare_CA.pem";
+      };
+      "CurlRC" = {
+        text = ''
+        --cacert /etc/ssl/certs/ca-certificates.crt
+        '';
+        target = "${homePrefix}/${user}/.curlrc";
+      };
+      "gh-ssh" = {
+        text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvhX5961G5kV7a9/p6nEBriBRUqE691VCcRDkot8EXD";
+        target = "hm_dummy/gh-ssh";
+        onChange = ''
+        rm -f ${homePrefix}/${user}/.ssh/id_ed25519
+        cp ${homePrefix}/${user}/hm_dummy/gh-ssh ${homePrefix}/${user}/.ssh/id_ed25519
+        chmod 0600 ${homePrefix}/${user}/.ssh/id_ed25519
+        '';
+      };
+      "glab-work" = {
+        text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMuboq7Wpr2+0SIZoq+MeGW2+5BcvOYnA0k5a6+rvqvC";
+        target = "hm_dummy/glab-work";
+        onChange = ''
+        rm -f ${homePrefix}/${user}/.ssh/glab-work
+        cp ${homePrefix}/${user}/hm_dummy/glab-work ${homePrefix}/${user}/.ssh/glab-work
+        chmod 0600 ${homePrefix}/${user}/.ssh/glab-work
+        '';
+      };
+    };
     username = user;
     homeDirectory = lib.mkForce "${homePrefix}/${user}";
     stateVersion = version;
--- hosts/darwinM/NWL-STUDIO.nix
+++ hosts/darwinM/NWL-STUDIO.nix
@@ -1,9 +1,28 @@
-{ config, user, ... }:
+{ pkgs, config, user, ... }:
+let
+  homePrefix =
+    if pkgs.stdenv.isDarwin
+    then "/Users"
+    else "/home";
+in
 {
   config = {
     home-manager = {
       users.${user} = {
-        home.file.".ssh/id_rpi_ed25519".source = ../../files/id_rpi_ed25519.pub;
+        home = {
+          file = {
+            "rpi-ssh" = {
+              text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdSCz3gWpimqj5AiJ5acxGKyaP0hfaZLDUTR+K35dJW";
+              target = "hm_dummy/rpi-ssh";
+              onChange = ''
+              rm -f ${homePrefix}/${user}/.ssh/rpi-ssh
+              cp ${homePrefix}/${user}/hm_dummy/rpi-ssh ${homePrefix}/${user}/.ssh/rpi-ssh
+              chmod u+rw ${homePrefix}/${user}/.ssh/rpi-ssh
+              '';
+            };
+          };
+        };
+
         programs = {
           ssh = {
             matchBlocks = {
