1274675f0b684c10b34d5074d84fd8c7cebf896e
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2025-06-27 10:21:47 -0400

    Updating Wezterm configuration

--- home/apps/wezterm/config.lua
+++ home/apps/wezterm/config.lua
@@ -1,66 +1,119 @@
 local wz = require("wezterm")
 local act = wz.action
 
-local config = {
-	-- Font
-	font = wz.font_with_fallback({
-    "JetBrains Mono",
-		"Source Code Pro",
-
-		-- For Symbols
-		{ family = "Symbols Nerd Font Mono", scale = 0.8 },
-		{ family = "PowerlineSymbols", scale = 0.8 },
-		{ family = "Noto Color Emoji", scale = 0.8 },
-	}),
-
-	font_size = 13.0,
-	allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
-	adjust_window_size_when_changing_font_size = false,
-	warn_about_missing_glyphs = true,
-
-	-- Colors
-	color_scheme = "@d.theme@",
-
-	-- Cursor
-	default_cursor_style = "SteadyBlock",
-	cursor_blink_ease_in = "Constant",
-	cursor_blink_ease_out = "Constant",
-	cursor_blink_rate = 500,
-
-	scrollback_lines = 10000,
-
-	-- https://sw.kovidgoyal.net/kitty/keyboard-protocol/
-	enable_kitty_keyboard = true,
-
-	-- Window
-	enable_wayland = false,
-	window_padding = {
-		left = "0cell",
-		right = "0cell",
-		top = "0cell",
-		bottom = "0cell",
-	},
-
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW",
-  native_macos_fullscreen_mode = true,
-
-	-- Tabs
-	use_fancy_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = false,
-
-	-- Keybindings
-	disable_default_key_bindings = false,
+-- List of remote hosts to open tabs for
+local remote_hosts = {
+  {
+    ssh_user     = "@d.user@",
+    ssh_host     = "hades.nwlnexus.net",
+    tmux_session = "@d.user_host@",
+  },
+  -- Add more hosts if desired
+  -- {
+  --   ssh_user      = "pi",
+  --   ssh_host      = "192.168.248.10",
+  --   tmux_session  = "pi-cluster",
+  -- },
 }
 
--- MacOS overrides
-if string.match(wz.target_triple, "apple") then
-	config.window_decorations = "TITLE | RESIZE"
+-- Helper to build startup tabs
+local function build_startup_tabs()
+  local tabs = {
+    {
+      args = { wz.executable }, -- local shell
+      title = wz.hostname(),
+    },
+  }
 
-	-- 	Use Command instead of Ctrl on MacOS
-	-- 	for k, v in pairs(config.keys) do
-	-- 		v.mods = string.gsub(v.mods, "CTRL", "CMD")
-	-- 	end
+  for _, host in ipairs(remote_hosts) do
+    table.insert(tabs, {
+      args = {
+        "ssh",
+        string.format("%s@%s", host.ssh_user, host.ssh_host),
+        "-t",
+        string.format("tmux new-session -A -s %s", host.tmux_session),
+      },
+      title = host.ssh_host,
+    })
+  end
+
+  return tabs
 end
+
+-- Merge with your config
+local config = {
+  -- Font
+  font = wz.font_with_fallback({
+    "JetBrains Mono",
+    "Source Code Pro",
+    { family = "Symbols Nerd Font Mono", scale = 0.8 },
+    { family = "PowerlineSymbols",       scale = 0.8 },
+    { family = "Noto Color Emoji",       scale = 0.8 },
+  }),
+  font_size = 13.0,
+  allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
+  adjust_window_size_when_changing_font_size = false,
+  warn_about_missing_glyphs = true,
+
+  -- Colors
+  color_scheme = "@d.theme@",
+
+  -- Cursor
+  default_cursor_style = "SteadyBlock",
+  cursor_blink_ease_in = "Constant",
+  cursor_blink_ease_out = "Constant",
+  cursor_blink_rate = 500,
+
+  scrollback_lines = 10000,
+  enable_kitty_keyboard = true,
+
+  -- Window
+  enable_wayland = false,
+  window_padding = {
+    left = "0cell",
+    right = "0cell",
+    top = "0cell",
+    bottom = "0cell",
+  },
+  window_close_confirmation = "NeverPrompt",
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW",
+  native_macos_fullscreen_mode = true,
+
+  -- Tabs
+  use_fancy_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = false,
+
+  -- Keybindings
+  disable_default_key_bindings = false,
+
+  -- Startup behavior
+  default_gui_startup_args = { "start" },
+}
+
+-- macOS override
+if string.match(wz.target_triple, "apple") then
+  config.window_decorations = "TITLE | RESIZE"
+end
+
+-- On startup, spawn all defined tabs
+wz.on("gui-startup", function()
+  local tabs = build_startup_tabs()
+  local _, _, window = wz.mux.spawn_window({})
+
+  for _, tab_info in ipairs(tabs) do
+    local tab = window:spawn_tab {
+      args = tab_info.args,
+    }
+    tab:set_title(tab_info.title)
+  end
+
+  -- Close the default empty tab
+  window:active_tab():kill()
+end)
+
+-- Dynamic tab titles: show current program/tmux title
+wz.on("format-tab-title", function(tab)
+  return { { Text = tab.active_pane.title or tab:get_title() } }
+end)
 
 return config
--- home/apps/wezterm/default.nix
+++ home/apps/wezterm/default.nix
@@ -1,13 +1,28 @@
-{ lib, theme, ... }:
+{
+  lib,
+  theme,
+  user,
+  config,
+  ...
+}:
 
 let
   themes = {
     "catppuccin" = "Catppuccin Mocha";
   };
 
-  config = builtins.replaceStrings
-    [ "@d.theme@" ] [ themes.${theme} ]
-    (lib.readFile ./config.lua);
+  # Try to get hostname from system config
+  hostname = config.networking.hostName or (builtins.getEnv "HOSTNAME");
+
+  replacements = {
+    "@d.theme@" = themes.${theme};
+    "@d.user@" = "${user}";
+    "@d.user_host@" = "${user}@${hostname}";
+  };
+
+  luaConfig =
+    builtins.replaceStrings (builtins.attrNames replacements) (builtins.attrValues replacements)
+      (lib.readFile ./config.lua);
 in
 
 {
@@ -15,5 +30,5 @@ in
     TERMINAL = "wezterm";
   };
 
-  xdg.configFile."wezterm/wezterm.lua".text = config;
+  xdg.configFile."wezterm/wezterm.lua".text = luaConfig;
 }
